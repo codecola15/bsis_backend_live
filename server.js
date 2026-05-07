d75a16e0ceee3cd2d9f7e89e600d7699591b8769
Jerwin Cruz <cruz.jerwin15@gmail.com>
2026-05-07 16:11:01 +0800

    Adding cors module

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const PORT = process.env.PORT || 8006;
 const cors = require('cors');
+const pool = require('./config/db');
 // Declarition of all routes
 const userRoute = require('./routes/userRoute');
 const studentRoute = require('./routes/studentRoute');
 
 const app = express();
+
 
 //Middleware
 app.use(express.json());
@@ -22,5 +24,13 @@ app.use('/api/students',studentRoute);
 // app.use('/api/departments',userRoute);
 
 app.listen(PORT,()=>{
+   app.listen(PORT, async () => {
+    try {
+        await pool.getConnection(); // 👈 test if DB is reachable
+        console.log('Database connected successfully');
+    } catch (err) {
+        console.error('Database connection failed:', err.message);
+    }
     console.log(`Server is running on port ${PORT}`);
+});
 })
